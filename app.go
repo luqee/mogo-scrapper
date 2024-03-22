2f731fa83b3a362fef3494ed802869624e6ce95c
Loki <saintlukke@gmail.com>
2024-03-22 18:34:48 +0300

    setup funcs

--- app.go
+++ app.go
@@ -2,23 +2,53 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
+
+	"golang.org/x/net/html"
 )
 
-func main() {
+func fetchUrl(url string) (*html.Node, error) {
 	client := http.Client{}
-	res, err := client.Get("https://cars.mogo.co.ke/auto/8367/nissan-x-trail-2003")
+	res, err := client.Get(url)
 	if err != nil {
 		fmt.Println(err)
 	}
 	if res.StatusCode != 200 {
 		fmt.Println("None 200 status code", res.StatusCode)
 	}
-	body, err := io.ReadAll(res.Body)
-	res.Body.Close()
+	defer res.Body.Close()
+	parsedDoc, err := html.Parse(res.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("%s", body)
+	return parsedDoc, nil
+}
+
+func getCarsList() ([]string, error) {
+	res, err := fetchUrl("https://cars.mogo.co.ke/auction")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Print(res)
+	cars := make([]string, 0)
+	cars = append(cars, "https://cars.mogo.co.ke/auto/8367/nissan-x-trail-2003")
+	return cars, nil
+}
+
+func getCarsDeets(carsList []string) error {
+	res, err := fetchUrl(carsList[0])
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Print(res)
+	return nil
+}
+
+func main() {
+	aucList, err := getCarsList()
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	getCarsDeets(aucList)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/luqee/mogo-auction-crawler
 
 go 1.21.1
+
+require golang.org/x/net v0.22.0 // indirect
