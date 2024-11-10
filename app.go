3be01b938dead315547d6a4cf29fda00363f2ac9
Loki <saintlukke@gmail.com>
2024-11-10 20:32:44 +0300

    Set up db

--- app.go
+++ app.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 	"golang.org/x/net/html"
 )
 
@@ -15,6 +23,7 @@ func fetchUrl(url string) (*html.Node, error) {
 	}
 	if res.StatusCode != 200 {
 		fmt.Println("None 200 status code", res.StatusCode)
+		return nil, errors.New("Not found")
 	}
 	defer res.Body.Close()
 	parsedDoc, err := html.Parse(res.Body)
@@ -24,31 +33,127 @@ func fetchUrl(url string) (*html.Node, error) {
 	return parsedDoc, nil
 }
 
-func getCarsList() ([]string, error) {
-	res, err := fetchUrl("https://cars.mogo.co.ke/auction")
-	if err != nil {
-		fmt.Println(err)
+type SearchAttr struct {
+	Key   string
+	Value string
+}
+
+func findNode(root *html.Node, elem string, attrib SearchAttr) *html.Node {
+	// fmt.Println(attrib.Value)
+	if root.Type == html.ElementNode && root.Data == elem {
+		for _, atr := range root.Attr {
+			if atr.Key == attrib.Key && strings.HasPrefix(atr.Val, attrib.Value) {
+				return root
+			}
+		}
 	}
-	fmt.Print(res)
-	cars := make([]string, 0)
-	cars = append(cars, "https://cars.mogo.co.ke/auto/8367/nissan-x-trail-2003")
-	return cars, nil
+	for c := root.FirstChild; c != nil; c = c.NextSibling {
+		foundNode := findNode(c, elem, attrib)
+		if foundNode != nil {
+			return foundNode
+		}
+	}
+	return nil
+}
+
+func extractCarUrls(root *html.Node, carUrls []string) {
+	var host = "https://cars.mogo.co.ke"
+	var dfs func(*html.Node)
+	dfs = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, atr := range n.Attr {
+				if atr.Key == "href" && strings.HasPrefix(atr.Val, "/auto/") {
+					carUrls = append(carUrls, host+atr.Val)
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			dfs(c)
+		}
+	}
+	dfs(root)
+}
+
+func extractDetails(root *html.Node) {
+
+	var dfs func(*html.Node)
+	dfs = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "div" {
+			for _, atr := range n.Attr {
+				if atr.Key == "class" && atr.Val == "font-bold" {
+					// get attributes
+					fmt.Println(n.FirstChild.Data)
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			dfs(c)
+		}
+	}
+	dfs(root)
+}
+
+func getCarsList(dbConn *pgx.Conn) ([]string, error) {
+	var pageNum = 1
+	carUrls := make([]string, 0)
+	for {
+		res, err := fetchUrl("https://cars.mogo.co.ke/auction?page=" + strconv.Itoa(pageNum))
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		extractCarUrls(res, carUrls)
+		pageNum++
+	}
+	return carUrls, nil
 }
 
 func getCarsDeets(carsList []string) error {
-	res, err := fetchUrl(carsList[0])
-	if err != nil {
-		fmt.Println(err)
+	for _, carUrl := range carsList {
+		res, err := fetchUrl(carUrl)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		aboutSection := findNode(res, "div", SearchAttr{Key: "class", Value: "vehicle-about__details"})
+		extractDetails(aboutSection)
 	}
-	fmt.Print(res)
 	return nil
 }
 
 func main() {
-	aucList, err := getCarsList()
+	err := godotenv.Load()
 	if err != nil {
-		fmt.Print(err.Error())
+		log.Fatal("Error loading .env file")
 	}
 
-	getCarsDeets(aucList)
+	dbConn := connect()
+
+	// res, err := fetchUrl("https://cars.mogo.co.ke/auto/10514/toyota-sienta-2008")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// aboutSection := findNode(res, "div", SearchAttr{Key: "class", Value: "vehicle-about__details"})
+	// extractDetails(aboutSection)
+	// aucList, err := getCarsList(dbConn)
+	// if err != nil {
+	// 	fmt.Print(err.Error())
+	// }
+	// getCarsDeets(aucList)
+}
+
+func connect() *pgx.Conn {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	pingErr := conn.Ping(context.Background())
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+	return conn
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/luqee/mogo-auction-crawler
 
 go 1.21.1
 
-require golang.org/x/net v0.22.0 // indirect
+require golang.org/x/net v0.22.0
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+)
